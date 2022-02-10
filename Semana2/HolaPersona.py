06ccfa363761a90fae3f397b90ed8a732a4fc1a6
stanlybolanos <32721384+stanlybolanos@users.noreply.github.com>
2022-02-10 17:54:31 -0600

    Ultimos cambios semana 2

--- Semana2/HolaPersona.py
+++ Semana2/HolaPersona.py
@@ -1,6 +1,8 @@
-import datetime
+from datetime import datetime
 
 
 NombrePersona = input("Porfavor ingrese su nombre:")
 
 print("Hola, te envio un saludo",NombrePersona,"en la fecha:",datetime.today().strftime("%Y-%m-%d"))
+
+print('hola')
