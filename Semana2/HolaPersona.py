a291de7d6049a96356197ddf353de1c3e82cca3e
stanlybolanos <32721384+stanlybolanos@users.noreply.github.com>
2022-02-10 18:22:29 -0600

    Agregamos el settings

--- Semana2/HolaPersona.py
+++ Semana2/HolaPersona.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
-
 NombrePersona = input("Porfavor ingrese su nombre:")
 
 print("Hola, te envio un saludo",NombrePersona,"en la fecha:",datetime.today().strftime("%Y-%m-%d"))
 
-print('hola')
