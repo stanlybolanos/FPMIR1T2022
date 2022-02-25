8c6d92a373eb2f0f0706f0fcbb15a0f82ed980c1
Axel Dardon <axeldardon@gmail.com>
2022-02-24 21:18:55 -0600

    ComoFuncion

--- GetHomework/SerieFibonacciAxelDardon.py
+++ GetHomework/SerieFibonacciAxelDardon.py
@@ -1,21 +1,23 @@
-Num1=int(input("Ingrese Ultimo Numero a generar:"))
-anterior=0
-actual=1
-i=actual
-if Num1>0:
-    if Num1==1:
-        print(anterior,", ",actual)
-    else:
-        print(anterior,", ",actual,end=", ")
-    while i<Num1:
-        siguiente=anterior+actual
-        i=siguiente
-        anterior=actual
-        actual=siguiente
-        if i>=Num1:
-            print(i)
+def Fibonacci1(ElNum):
+    anterior=0
+    actual=1
+    i=actual
+    if Num1>0:
+        if Num1==1:
+            print(str(anterior) + ", "+str(actual))
         else:
-            print(i, end=", ")
-else:
-    print("No es posible realizar la secuencia")
-    
+            print(str(anterior)+ ", "+str(actual),end=", ")
+        while i<Num1:
+            siguiente=anterior+actual
+            i=siguiente
+            anterior=actual
+            actual=siguiente
+            if i>=Num1:
+                print(i)
+            else:
+                print(i, end=", ")
+    else:
+        print("No es posible realizar la secuencia")
+
+Num1=int(input("Ingrese Ultimo Numero a generar:"))
+Fibonacci1(Num1)    
