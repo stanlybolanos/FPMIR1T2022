82e419502607fe1f9015ad7063c2d4d80a56d128
Axel Dardon <axeldardon@gmail.com>
2022-03-02 20:05:38 -0600

    PruebaFinalTarea5

--- GetHomework/GetHomeworkTarea5-AxelDardon.py
+++ GetHomework/GetHomeworkTarea5-AxelDardon.py
@@ -2,28 +2,33 @@ import webbrowser
 import requests
 import json
 from SubirArchivosGitHub import *
-NoSeContinua=False
-UsuarioCancelo=False
+
 #Esta funcion devuelve la tarea de la semana al recibir el numero de semana como parametro
 def GetHomework(SemanaN,FilePath):
+    NoSeContinua = False
     #Parte I
     Enalce="https://fpmir.azurewebsites.net/{}/AZFMIR?AZFNUM={}".format("api",SemanaN)
-    webbrowser.open(Enalce, new=2)
+    #webbrowser.open(Enalce, new=2)
     ##Parte II
     response=requests.get(Enalce).text
     if response == "El Key introducido es invalido o aun no esta disponible":
         print("No hay contenido para esta semana")
-        exit()
-    try:
-        response_info=json.loads(response)
-    except:
-        print("Hubo un error en la construcción del archivo JSON")
-        exit()
+        NoSeContinua=True
     else:
-        ArchivoPath = FilePath
-        archivo=open(FilePath, "w")
-        json.dump(response_info,archivo,indent=6)
-        archivo.close()
+        try:
+            response_info=json.loads(response)
+        except:
+            print("Hubo un error en la construcción del archivo JSON")
+            NoSeContinua=True
+        else:
+            ArchivoPath = FilePath
+            archivo=open(FilePath, "w")
+            json.dump(response_info,archivo,indent=6)
+            archivo.close()
+    if NoSeContinua:
+        return False
+    else:
+        return True
 def ExisteElDir(QueArchivo):
     #ElPathName = os.path.dirname(QueArchivo)
     if os.path.exists(QueArchivo):
@@ -31,6 +36,7 @@ def ExisteElDir(QueArchivo):
     else:
         print("No se encontró el path para guardar los archivos, no se puede seguir adelante ")
         return False
+UsuarioCancelo=False
 InputFile = input("Ingrese path para los archivos:")
 if ExisteElDir(InputFile):
     #ElPath = os.path.dirname(InputFile)
@@ -47,16 +53,22 @@ if ExisteElDir(InputFile):
         MensajeCommit = input("Ingrese el mensaje para commit: ")
         NombreRepo = input("Ingrese el nombre para el repositorio: ")
         x=1
-        while x<= InputSemanaNum and not NoSeContinua:
+        TerminoAntes=False
+        while x<= InputSemanaNum and not TerminoAntes:
             InputSemana = "Semana" + str(x).strip()
             InputSemanaFile = InputSemana + ".json"
             ElFile = InputFile + os.sep + InputSemanaFile
-            GetHomework(InputSemana,ElFile)
-            if not NoSeContinua:
+            if GetHomework(InputSemana,ElFile):
                 SubirGitHub(MensajeCommit,TokenGithub,ElFile,NombreRepo)
                 x+=1
             else:
+                TerminoAntes=True
                 break
+        if TerminoAntes:
+            #Hubo error al querer guardar el archivo de la semana x
+            print("Se descargaron archivos hasta la semana ",x-1)
+        else:
+            print("Se lograron descargar todas las semanas exitosamente")
 else:
     exit()
 
