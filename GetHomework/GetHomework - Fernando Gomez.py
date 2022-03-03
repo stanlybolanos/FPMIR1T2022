8611d7a2cfd77b44f2d11e5fd70ea912cdee6778
Fernando Gomez <fgomez1211@gmail.com>
2022-03-03 16:59:00 -0600

    Tarea GH5

--- GetHomework/GetHomework - Fernando Gomez.py
+++ GetHomework/GetHomework - Fernando Gomez.py
@@ -9,52 +9,59 @@ from SubirArchivosGitHub import SubirGitHub
 #Esta funcion devuelve la tarea de la semana al recibir el numero de semana como parametro
 def GetHomework(SemanaN,FilePath):
     #Parte I
+
+  for x in range(1,Numero+1):
+    SemanaN="Semana"+str(x)
+    FilePath=Direccion+"/Semana"+str(x)+".json"
+
     Enlace="https://fpmir.azurewebsites.net/{}/AZFMIR?AZFNUM={}".format("api",SemanaN)
     webbrowser.open(Enlace, new=2)
     #Parte II
     response=requests.get(Enlace).text
-    response_info=json.loads(response)
-    archivo=open(FilePath, "w")
-    #Parte IV
+    
+    if(response=="El key introducido es invalido o aun no esta disponible"):
+        print("No hay contenido disponible para esta semana")
+        print("Se han grabado correctamente los archivos hasta la semana:",str(x-1))
+        exit()
+
     try:
+        response_info=json.loads(response)
+        archivo=open(FilePath, "w")
         json.dump(response_info,archivo,indent=6)
+
     except:
-        print("Ocurrió un problema al crear el archivo JSON")
-    else:
-        print("El archivio ha sido creado exitosamente")
-    finally:
-        archivo.close()
-
-InputSemana = input("Ingrese semana: ")
-#Parte IV
-#Verificación del API para el contenido de la semana.
-DireccionAPI = requests.get("https://fpmir.azurewebsites.net/{}/AZFMIR?AZFNUM={}".format("api",InputSemana))
-if (DireccionAPI.text=="El Key introducido es invalido o aun no esta disponible"):
-    print("No hay contenido para esta semana")
-    exit()
+        print("No se puede modificar el archivo")
+    
+    else:      
+        print("Se almaceno exitosamente al archivo:", FilePath)
+    
+    finally:  
+        if(x==SemanaN):
+            print("Se han descargado",SemanaN,"Semanas de forma exitosa")
+    archivo.close()
+            
 
 
-InputFile = input("Ingrese\ path y nombre de archivo: ")
-#Parte IV
-#Verificación de la extensión .json.
-root, extension = os.path.splitext(InputFile)
-if (extension!='.json'):
-    print("La extensión JSON es invalida.")
-    exit()
+Numero=int(input("Ingrese el numero de semanas: "))
+Direccion=input("Ingrese Path: ")
+GetHomework(Numero,Direccion)
 
-#Verificación del Filepath
-if os.path.exists(InputFile):
-   print("El path si existe. ")
+
+
+
+"""
+if(os.path.isdir(os.path.dirname(Direccion))==False):
+    print("El path ingresado:", os.path.dirname(Direccion),"No se encuentra, ingrese un path adecuado.")
 else:
-   print("El path no existe, se procederá a salir.")
-   exit()
 
 
-GetHomework(InputSemana,InputFile)
+#Parte IV
+#Verificación del Filepath
 
 Mensaje = input("Ingrese el mensaje del Commit: ")
 TokenGithub = input("Ingrese Token para autenticarse a GitHub: ")
 Repo = input("Ingrese el Repo de GitHub: ")
 
 SubirGitHub(MensajeCommit=Mensaje, Token=TokenGithub, Archivo=InputFile, NombreRepo=Repo)
+"""
 
